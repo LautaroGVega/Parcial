5aed576e2dd7b2800a3e5cb6f9cd75cf50206811
LautaroGVega <vegalautaro177@gmail.com>
2023-11-04 23:09:39 -0300

    agregar test3

--- ParcialParadigmas/demo/src/test/java/Parcial1Tests.java
+++ ParcialParadigmas/demo/src/test/java/Parcial1Tests.java
@@ -86,6 +86,23 @@ import org.junit.Test;
         assertEquals("Semifinal Partido Vuelta BOCxPAL", partido2Vuelta.getName());
 
     }
+    @Test
+    public void torneo_agregar_jugadores_a_un_equipo_parcial_ejercicio_ejemplo_03()
+    {        
+        Equipo boca = new Equipo("Boca Juniors", "BOC");          
+        
+        Jugador jugador10 = new Jugador("Edinson Cavani", 10);   
+        Jugador jugador19 = new Jugador("Valentin Barco", 19);
+  
+        boca.agregar(jugador10);         
+        boca.agregar(jugador19); 
+
+        assertEquals(2, boca.jugadoresCantidad());          
+        
+        assertEquals("Edinson Cavani", boca.obtenerJugador(10).getName());          
+        assertEquals("Valentin Barco", boca.obtenerJugador(19).getName());  
+
+    }
         @Test
     public void torneo_sacar_tarjeta_roja_a_un_jugador_en_un_partido_parcial_ejercicio_ejemplo_04()
     {        
